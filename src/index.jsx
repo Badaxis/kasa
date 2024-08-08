4eedc25097893bafbd1e4e092598952cf1a6b7a6
Vincent Billet <billet.vincent@free.fr>
2024-08-08 14:23:21 +0200

    Derniers ajouts, structure projet

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 import Error from "./pages/Error";
 import About from "./pages/About";
 import Housing from "./pages/Housing";
-import "./styles/index.scss";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
